517c7ff8d5b8f9cba69568a17a77e0cebc96c745
Stefan Gussner <e1527253@student.tuwien.ac.at>
2019-04-02 18:13:01 +0200

    git can now stage files

--- src/lib/git.ts
+++ src/lib/git.ts
@@ -1,127 +1,164 @@
 import * as fs from 'fs';
-import {promisify} from 'util';
+import { promisify } from 'util';
 import * as path from 'path';
 import { spawn } from 'child_process';
 
 const readFile = promisify(fs.readFile);
 const readDir = promisify(fs.readdir);
 
-const repositories:Map<string, string> = new Map();
+/**
+ * Promies wrapper around the spawn interface
+ * @param process process to start
+ * @param args arguments
+ * @param options options
+ */
+function promiseSpawn(process, args, options = {}): Promise<string> {
+    return new Promise(async (resolve, reject) => {
+        const proc = spawn(process, args, options);
+        let output = '';
+        proc.stdout.on('data', data => {
+            output += data.toString();
+        });
+        proc.stderr.on('data', d => console.error(d.toString()));
+        proc.on('exit', code => code != 0 ? reject('unexpected error') : resolve(output));
+    });
+}
+
+const repositories: Map<string, string> = new Map();
 
 /**
  * This is essentially cd .. for path strings
  * @param path A unix style path
  * @returns the path to the parent directory
  */
-function dirUp(path:string){
+function dirUp(path: string) {
     const parts = path.split(/\//g);
-    for(let i = parts.length-1;i>0;i--){
-        if(parts[i]==''){
+    for (let i = parts.length - 1; i > 0; i--) {
+        if (parts[i] == '') {
             continue;
         }
-        return parts.slice(0,i).join('/');
+        return parts.slice(0, i).join('/');
     }
 }
 
-export async function findGitDirectory(startpath:string):Promise<string>{
+export async function findGitDirectory(startpath: string): Promise<string> {
     let currpath = path.dirname(startpath).toString();
-    if(repositories.has(startpath)){
+    if (repositories.has(startpath)) {
         return repositories.get(startpath);
     }
     let found = false;
-    while(currpath.length>0){
+    while (currpath.length > 0) {
         const dir = await readDir(currpath);
-        if(dir.indexOf('.git')>-1){
-            repositories.set(startpath,currpath+'/.git/');
-            return currpath+'/.git/';
+        if (dir.indexOf('.git') > -1) {
+            repositories.set(startpath, currpath);
+            return currpath;
         }
-        currpath=dirUp(currpath);
+        currpath = dirUp(currpath);
     }
     throw new Error('Not a git directory');
 }
 
-
-export async function getCurrentBranch(path){
-    const gitDir:String = await findGitDirectory(path);
-    return (await readFile(gitDir+'HEAD')).toString().match(/ref: (.*)/)[1];
+export async function getCurrentBranch(path) {
+    const gitDir: String = await findGitDirectory(path);
+    return (await readFile(gitDir + '/.git/HEAD')).toString().match(/ref: (.*)/)[1];
 }
 
-function readGitObject(hash:string, repositoryPath:string):Promise<string>{
-    return new Promise((resolve,reject)=>{
-        const git = spawn('git', ['cat-file', '-p', hash],{cwd:repositoryPath});
-        let output='';
-        git.stdout.on('data',data=>{
-           output+=data.toString(); 
-        });
-        git.stderr.on('data',d=>console.error(d.toString()));
-        git.on('exit',code=>code!=0?reject('unexpected error'):resolve(output));
-    });
+function readGitObject(hash: string, repositoryPath: string): Promise<string> {
+    return promiseSpawn('git', ['cat-file', '-p', hash], { cwd: repositoryPath })
 }
 
-async function getFilePathRelativeToRepo(filename:string):Promise<string>{
-    let dir:string = await findGitDirectory(filename);
-    dir = dirUp(dir);
+async function getFilePathRelativeToRepo(filename: string): Promise<string> {
+    const dir: string = await findGitDirectory(filename);
     return filename.substr(dir.length);
 }
 
-enum gitObjectType{
+enum gitObjectType {
     tree,
     blob,
     content
 }
 
-function parseGitObject(content:string){
+function parseGitObject(content: string) {
     const lines = content.split(/\n/g);
-    
+
     //if this is a commit
     let typeIndex = 1;
 
     const parsedContent = [];
 
-    if(content.startsWith('tree')){
-        typeIndex=0;
+    if (content.startsWith('tree')) {
+        typeIndex = 0;
     }
-    for(let l of lines){
+    for (let l of lines) {
         const line = l.split(/\s/g);
-        switch(line[typeIndex]){
+        switch (line[typeIndex]) {
             case 'tree':
-                parsedContent.push({type:gitObjectType.tree, hash:line[typeIndex+1], name:line[typeIndex+2]||''});
+                parsedContent.push({ type: gitObjectType.tree, hash: line[typeIndex + 1], name: line[typeIndex + 2] || '' });
                 break;
             case 'blob':
-                parsedContent.push({type:gitObjectType.blob, hash:line[typeIndex+1], name:line[typeIndex+2]||''});
+                parsedContent.push({ type: gitObjectType.blob, hash: line[typeIndex + 1], name: line[typeIndex + 2] || '' });
                 break;
             default:
-                if(parsedContent.length==0){
-                    return [{type:gitObjectType.content,content}];
+                if (parsedContent.length == 0) {
+                    return [{ type: gitObjectType.content, content }];
                 }
         }
     }
     return parsedContent;
 }
 
-export async function getCurrentFileVersion(filename:string):Promise<string>{
+export async function getCurrentFileVersion(filename: string): Promise<string> {
     const gitDir = await findGitDirectory(filename);
     let commit = await getCurrentBranch(filename);
-    const relativeFilePath:string = await getFilePathRelativeToRepo(filename);
+    const relativeFilePath: string = await getFilePathRelativeToRepo(filename);
     let currentObject = parseGitObject(await readGitObject(commit, gitDir));
     //get the tree of the git root
-    currentObject = parseGitObject(await readGitObject(currentObject.filter(o=>o.type==gitObjectType.tree)[0].hash, gitDir));
-    for(let dir of relativeFilePath.split(/\//g)){
-        if(dir==''){
+    currentObject = parseGitObject(await readGitObject(currentObject.filter(o => o.type == gitObjectType.tree)[0].hash, gitDir));
+    for (let dir of relativeFilePath.split(/\//g)) {
+        if (dir == '') {
             continue;
         }
-        const ref = currentObject.filter(o=>o.name==dir)[0];
-        if(!ref){
+        const ref = currentObject.filter(o => o.name == dir)[0];
+        if (!ref) {
             throw new Error('file not found in repo');
         }
-        if(ref.type==gitObjectType.tree){
+        if (ref.type == gitObjectType.tree) {
             currentObject = parseGitObject(await readGitObject(ref.hash, gitDir));
         }
-        else if(ref.type==gitObjectType.blob){
+        else if (ref.type == gitObjectType.blob) {
             return await readGitObject(ref.hash, gitDir);
         }
-        else{
+        else {
             return currentObject[0].content;
         }
     }
 }
+
+export async function getGitHash(content: string) {
+    return new Promise(async (resolve, reject) => {
+        const proc = spawn('git', ['hash-object', '--stdin']);
+        let output = '';
+        proc.stdout.on('data', data => {
+            output += data.toString();
+        });
+        proc.stderr.on('data', d => console.error(d.toString()));
+        proc.on('exit', code => code != 0 ? reject('unexpected error') : resolve(output));
+
+        proc.stdin.write(content);
+        proc.stdin.end();
+    });
+}
+
+async function addGitObject(content: string, repo): Promise<string> {
+    return await promiseSpawn('git', ['hash-object', '--stdin', content], { cwd: repo });
+    //git hash-file --stdin <content>
+}
+
+export async function stageGitObject(filename: string, content: string) {
+    const path = await getFilePathRelativeToRepo(filename);
+    const repositoryPath = await findGitDirectory(filename);
+    const hash = await addGitObject(content, repositoryPath);
+    await promiseSpawn('git', ['udpate-index', '--add', '--cacheinfo', '100644', hash, path], { cwd: repositoryPath });
+    //git update-index --add --cacheinfo 100644 93821e8182534e2d95df1acc85fa589556dd61dc contributors.txt 
+}
+
---
+++ src/test/lib/git.test.ts
@@ -0,0 +1,40 @@
+import * as git from '../../lib/git';
+import * as fs from 'fs';
+import { promisify } from 'util';
+import { spawn } from 'child_process';
+import * as rimraf from 'rimraf';
+
+
+function promiseSpawn(process, args, options = {}): Promise<string> {
+    return new Promise(async (resolve, reject) => {
+        const proc = spawn(process, args, options);
+        let output = '';
+        proc.stdout.on('data', data => {
+            output += data.toString();
+        });
+        proc.stderr.on('data', d => console.error(d.toString()));
+        proc.on('exit', code => code != 0 ? reject('unexpected error') : resolve(output));
+    });
+}
+
+const testDir = __dirname+'/test_git';
+beforeAll(async () => {
+    fs.mkdirSync(testDir);
+    console.log(await promiseSpawn('git', ['init'],{cwd:testDir}));
+    fs.writeFileSync(testDir+'/test.txt','das ist ein test');
+});
+
+afterAll(done=>{
+    rimraf(testDir,done)
+})
+
+
+test('getHash', async () => {
+    const hash = await git.getGitHash('Stefan Gussner');
+    expect(hash).toMatch('93821e8182534e2d95df1acc85fa589556dd61dc');
+});
+
+test('findGitDirectory', async ()=>{
+    const dir = await git.findGitDirectory(testDir+'/test.txt');
+    expect(dir).toMatch(testDir);
+});
